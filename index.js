2ef2f8a079ca38311a43adbe26a5cc173e9b9e1e
Meklit Cherie <makywonde347@yahoo.com>
2021-05-12 15:37:46 -0700

    finished challenge 1

---
+++ index.js
@@ -0,0 +1,24 @@
+var data = require("./data.json");
+// Helper Functions //
+function isEmpty(str) {
+    var words = str.trim().split('');
+    for (var i = 0; i < words.length; i += 1) {
+        if (words[i] !== ('\n' || '\r' || '\t')) {
+            return false;
+        }
+    }
+    return true;
+}
+function upperFirst(str) {
+    if (isEmpty(str) === true) {
+        return '';
+    }
+    var strEndSlice = str.slice(1);
+    var firstLetter = str[0].toUpperCase();
+    return firstLetter + strEndSlice;
+}
+// Solution //
+function printData() {
+    console.log(upperFirst(data[0].first_name) + " " + upperFirst(data[0].last_name));
+}
+printData();
