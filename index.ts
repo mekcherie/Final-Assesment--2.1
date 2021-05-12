9424f1c6bc6b2bccd157e47e82a48af06e6cfad4
Meklit Cherie <makywonde347@yahoo.com>
2021-05-12 16:35:42 -0700

    finished challenge 2, 3, 4, 5, 6

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 const data = require("./data.json");
 
-// Helper Functions //
 function isEmpty(str: string): boolean {
   const words: string[] = str.trim().split('');
   for (let i: number = 0; i < words.length; i += 1) {
@@ -19,10 +18,22 @@ function upperFirst(str: string): string {
   const firstLetter: string = str[0].toUpperCase();
   return firstLetter + strEndSlice;
 }
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+function formatNumber(number: string): string {
+    const areaCode: string = number.slice(0, 3)
+    const middle: string = number.slice(3, 6)
+    const end: string = number.slice(6)
+    return `(${areaCode}) ${middle}-${end}`
+  }
 
-// Solution //
 function printData() {
-  console.log(`${upperFirst(data[0].first_name)} ${upperFirst(data[0].last_name)}`)
+    console.log(upperFirst(data[0].first_name) + " " + upperFirst(data[0].last_name));
+    console.log(upperFirst(data[0].make) + " " + upperFirst(data[0].model));
+    const purchaseDate = new Date(data[0].purchased);
+    console.log(`${months[purchaseDate.getMonth()]} ${purchaseDate.getDate()}, ${purchaseDate.getFullYear()}`)
+
+  console.log(formatNumber(data[0].phone))
 }
 
-printData()
+
+printData();
